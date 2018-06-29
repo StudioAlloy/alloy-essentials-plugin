a25838c84ca6912090cf391a4f1c3be1d8a0d439
Mitchel van Eijgen <mvaneijgen@gmail.com>
2018-06-29 11:18:59 +0200

    Update alloy-essentials.php

--- alloy-essentials.php
+++ alloy-essentials.php
@@ -4,7 +4,7 @@ Plugin Name: Alloy Essentials
 Plugin URI:
 Github Plugin URI: https://github.com/StudioAlloy/studio-alloy-essentials
 Description: All the essential settings for a standard Studio Alloy wordpress project
-Version: 1.0.7
+Version: 1.0.8
 Author: Studio Alloy
 Author URI: https://studioalloy.nl/
 License: GPL2
